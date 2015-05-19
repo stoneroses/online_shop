94a9ab6d1212832d24e3de343ce7983086a754db
michaelwangbrisbane <michael.wang.brisbane@gmail.com>
2015-05-19 10:40:54 +1000

    Fixed testimonial table issue

--- src/main/sql/install/table/install/15.install.testimonial.sql
+++ src/main/sql/install/table/install/15.install.testimonial.sql
@@ -1,11 +1,13 @@
-CREATE TABLE `news` (
+CREATE TABLE `testimonials` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(256) NOT NULL DEFAULT '' COLLATE 'utf8_bin',
   `title` VARCHAR(256) NOT NULL DEFAULT '' COLLATE 'utf8_bin',
-  `content` VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8_bin',
+  `content` VARCHAR(2048) NOT NULL DEFAULT '' COLLATE 'utf8_bin',
   `createdDateTime` DATETIME NOT NULL,
   `updatedDateTime` DATETIME NULL DEFAULT NULL,
   INDEX `id` (`id`)
 )
 COLLATE='utf8_bin'
 ENGINE=InnoDB
+AUTO_INCREMENT=3
 ;
