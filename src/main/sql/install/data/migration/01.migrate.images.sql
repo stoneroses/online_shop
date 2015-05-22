a9de02318ff10128135a1e3c602323bf783e9bf1
michaelwangbrisbane <michael.wang.brisbane@gmail.com>
2015-05-22 11:51:31 +1000

    Added images migration shell script.

--- src/main/sql/install/data/migration/01.migrate.images.sql
+++ src/main/sql/install/data/migration/01.migrate.images.sql
@@ -6,6 +6,10 @@ INSERT INTO online_shop_development.images
 )
 
 (
-  SELECT ozpigeon_zcdb.products.products_id, ozpigeon_zcdb.products.products_image, ozpigeon_zcdb.products.products_image, ozpigeon_zcdb.products.products_image, NOW()
+  SELECT ozpigeon_zcdb.products.products_id, ozpigeon_zcdb.products.products_image, ozpigeon_zcdb.products.products_image, CONCAT("/", ozpigeon_zcdb.products.products_image), NOW()
   FROM ozpigeon_zcdb.products
 );
+
+UPDATE online_shop_development.images SET online_shop_development.images.location = REPLACE(online_shop_development.images.location, ' ', '_');
+UPDATE online_shop_development.images SET online_shop_development.images.location = REPLACE(online_shop_development.images.location, '(', '_');
+UPDATE online_shop_development.images SET online_shop_development.images.location = REPLACE(online_shop_development.images.location, ')', '_');
