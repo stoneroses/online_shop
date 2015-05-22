ab5eea89ae2832d9cef4872752a156c8db7fc07b
michaelwangbrisbane <michael.wang.brisbane@gmail.com>
2015-05-22 23:04:00 +1000

    Added new preset user

--- src/main/sql/install/data/install/03.install.users.sql
+++ src/main/sql/install/data/install/03.install.users.sql
@@ -1,4 +1,4 @@
 DELETE FROM `users`;
 
 INSERT INTO `users` (`id`, `email`, `name`, `password`, `description`, `createdDateTime`, `updatedDateTime`) VALUES (1, 'michael.wang.brisbane@gmail.com', 'Michael Wang', '6ba174f917482b9e881938ff194ffb159c46b2807dd5b7e1342587210532be1b', 'Michael Wang', NOW(), NULL);
-INSERT INTO `users` (`id`, `email`, `name`, `password`, `description`, `createdDateTime`, `updatedDateTime`) VALUES (2, 'cnlancer@gmail.com', 'cnlancer', 'password', '7a8c8739df2906dfd866a776cca6f2a0355e2bdbe9743b614f7e3d07c263aa74', NOW(), NULL);
+INSERT INTO `users` (`id`, `email`, `name`, `password`, `description`, `createdDateTime`, `updatedDateTime`) VALUES (2, 'zhenyi.zhao@gmail.com', 'Zhenyi Pang', 'd335d06aa1db52a0db83821d6b4ce999bd86298278a5741032e915801c406299', 'Zhenyi Pang', NOW(), NULL);
--- src/main/sql/install/data/install/05.install.user_role.sql
+++ src/main/sql/install/data/install/05.install.user_role.sql
@@ -3,4 +3,5 @@ DELETE FROM `user_role`;
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES (1, 1);
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES (1, 2);
 
+INSERT INTO `user_role` (`user_id`, `role_id`) VALUES (2, 1);
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES (2, 2);
